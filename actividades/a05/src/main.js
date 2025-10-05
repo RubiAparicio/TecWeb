897d49a8e14808a28a2794ca5bbe909d9d44856e
RubiAparicio <rubiaparicio976@gmail.com>
2025-10-05 11:21:56 -0600

    Ejemplos JS02 completo

--- actividades/a05/src/main.js
+++ actividades/a05/src/main.js
@@ -15,3 +15,18 @@ function getDatos()
 function ejemplo1() {
     document.getElementById("resultado1").innerHTML = "Hola Mundo";
 }
+
+function ejemplo2() {
+    var nombre = 'Rubi';
+    var edad = 21;
+    var altura = 1.50;
+    var casado = false;
+
+    document.getElementById("resultado2").innerHTML = nombre + "<br>" + edad + "<br>" + altura + "<br>" + casado;
+}
+
+function ejemplo3() {
+    var nombre = prompt("Nombre: ", "");
+    var edad = prompt("Edad: ", "");
+    document.getElementById("resultado3").innerHTML = "Hola " + nombre + " así que tienes " + edad + " años";
+}
